c9766052421767149aa168402f09b436582098e8
mk_work <mknord1565@gmail.com>
2022-07-08 15:11:03 +0300

    improvements app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from utils import load_candidates, get_all, get_by_pk, get_by_skill
-from constants import picture_url, picture_sourse
+from constants import picture_url, picture_url_2
 
 candidates: list = load_candidates()
 
@@ -14,29 +14,12 @@ def all_candidates():
 
 @app.route("/candidates/<pk>")
 def candidates_by_pk(pk):
-    return f"<img src='({picture_sourse})'>, {get_by_pk(candidates, int(pk))}"
+    return f"<img src='({picture_url})'>, {get_by_pk(candidates, int(pk))}"
+
 
 @app.route("/skills/<skill>")
-def candidates_by_pk(skill):
+def candidates_by_skill(skill):
+    return get_by_skill(candidates, skill)
 
-
-
-# @app.route("/profile/")
-# def page_profile():
-#     return "Профиль пользователя"
-#
-# @app.route("/feed/")
-# def page_feed():
-#     return "Лента пользователя"
-#
-# @app.route("/messages/")
-# def page_messages():
-#     return "Сообщения пользователя"
-#
-#
-#
-# @app.route('/catalog/items/<itemid>')
-# def profile_goods(itemid):
-#     return f'<h1>Страничка товара {itemid}</h1>'
 
 app.run(host='127.0.0.2', port=80)
