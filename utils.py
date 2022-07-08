8324addb50b46003669b6d88f43aaf1f3f5e3fa3
mkN <mknord1565@gmail.com>
2022-07-08 20:18:46 +0300

    minor changes

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def get_all(candidates: list) -> str:
     candidates_list = []
     for candidate in candidates:
         candidates_list.append(f"\tИмя кандидата - {candidate['name']}\n"
-                               f"\tПозиция кандидата - {candidate['pk']}\n"
+                               f"\tПозиция кандидата - {candidate['position']}\n"
                                f"\tНавыки кандидата: {candidate['skills']}\n")
 
     return ("<pre>" + "\n" + "\n".join(candidates_list) + "</pre>")
@@ -29,7 +29,7 @@ def get_by_pk(candidates: list, pk: int) -> str:
         if candidate["pk"] == pk:
             return f"<pre>\n" \
                    f"\tИмя кандидата - {candidate['name']}\n" \
-                   f"\tПозиция кандидата - {candidate['pk']}\n" \
+                   f"\tПозиция кандидата - {candidate['position']}\n" \
                    f"\tНавыки кандидата: {candidate['skills']}\n" \
                    f"</pre>"
     return "No candidate with such pk"
@@ -42,7 +42,7 @@ def get_by_skill(candidates: list, skill_name: str) -> str:
     for candidate in candidates:
         if skill_name.strip().lower() in candidate["skills"].lower():
             candidates_by_skill.append(f"\tИмя кандидата - {candidate['name']}\n"
-                                       f"\tПозиция кандидата - {candidate['pk']}\n"
+                                       f"\tПозиция кандидата - {candidate['position']}\n"
                                        f"\tНавыки кандидата: {candidate['skills']}\n")
     if len(candidates_by_skill) > 0:
         return ("<pre>" + "\n" + "\n".join(candidates_by_skill) + "</pre>")
