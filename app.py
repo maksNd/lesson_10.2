6c6765cda7d3837aeb871640d24469aafa8b72af
mk_work <mknord1565@gmail.com>
2022-07-08 15:27:21 +0300

    minor changes

--- app.py
+++ app.py
@@ -3,24 +3,23 @@ from utils import load_candidates, get_all, get_by_pk, get_by_skill
 from constants import picture_url
 
 if __name__ == "__main__":
-
     candidates: list = load_candidates()
 
     app = Flask(__name__)
 
 
     @app.route("/")
-    def all_candidates():
+    def all_candidates() -> str:
         return get_all(candidates)
 
 
     @app.route("/candidates/<pk>")
-    def candidates_by_pk(pk):
+    def candidates_by_pk(pk) -> str:
         return f"<img src='({picture_url})'>, {get_by_pk(candidates, int(pk))}"
 
 
     @app.route("/skills/<skill>")
-    def candidates_by_skill(skill):
+    def candidates_by_skill(skill) -> str:
         return get_by_skill(candidates, skill)
 
 
