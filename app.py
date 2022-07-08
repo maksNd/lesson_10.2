e0ceeaf94d560730d3b524c9ece2e5c51c9f1212
mk_work <mknord1565@gmail.com>
2022-07-08 15:15:28 +0300

    improvements app.py

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 from flask import Flask
 from utils import load_candidates, get_all, get_by_pk, get_by_skill
-from constants import picture_url, picture_url_2
+from constants import picture_url
 
-candidates: list = load_candidates()
+if __name__ == "__main__":
 
-app = Flask(__name__)
+    candidates: list = load_candidates()
 
-
-@app.route("/")
-def all_candidates():
-    return get_all(candidates)
+    app = Flask(__name__)
 
 
-@app.route("/candidates/<pk>")
-def candidates_by_pk(pk):
-    return f"<img src='({picture_url})'>, {get_by_pk(candidates, int(pk))}"
+    @app.route("/")
+    def all_candidates():
+        return get_all(candidates)
 
 
-@app.route("/skills/<skill>")
-def candidates_by_skill(skill):
-    return get_by_skill(candidates, skill)
+    @app.route("/candidates/<pk>")
+    def candidates_by_pk(pk):
+        return f"<img src='({picture_url})'>, {get_by_pk(candidates, int(pk))}"
 
 
-app.run(host='127.0.0.2', port=80)
+    @app.route("/skills/<skill>")
+    def candidates_by_skill(skill):
+        return get_by_skill(candidates, skill)
+
+
+    app.run(host='127.0.0.2', port=80)
